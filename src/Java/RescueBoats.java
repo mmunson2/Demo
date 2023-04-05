da844959981f1fbbaeb353620c032b659559ad84
Matthew Munson <mmunson11@gmail.com>
2023-04-04 17:51:18 -0700

    Additional RescueBoats comments

--- src/Java/RescueBoats.java
+++ src/Java/RescueBoats.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 /********************************************************************************
+ * LeetCode Problem #881
+ * 
  * You are given an array people where people[i] is the weight of the ith person,
  * and an infinite number of boats where each boat can carry a maximum weight of limit.
  * 
@@ -12,16 +12,23 @@ import java.util.List;
  * 
  * Return the minimum number of boats to carry every given person.
  * 
- * Notes:
- * - My goal should be to pair two people that meet the limit exactly. If I can't find that, then check for
- * two people who meet limit - 1, and so on.
+ * ________________________________________________________
+ * My Solution:
  * 
- * - Sorting the people array will probably help.
+ * After 20 minutes of analysis paralysis, I went ahead with this implementation
+ * which:
  * 
- * - 
+ * - Sorts the array of people by weight
+ * - Attempts to match the heaviest remaining person with the lightest
  * 
+ * I did this with two pointers on the same array, i and j. i is the usual
+ * loop index. j starts at the end of the array and moves backwards. It
+ * only moves if we're able to match the heaviest and lightest. This results
+ * in four possible cases outlined in the code.
  * 
- * 
+ * The code for the cases could be shortened a bit (boatCount++ is called in all
+ * but one case), but I've left it as-is to make sure the reasoning behind each
+ * is clear.
  ********************************************************************************/
 
 class RescueBoats {
@@ -55,13 +62,12 @@ class RescueBoats {
                 j--;
             }
             // Case 4: The current heaviest person can't fit with the lightest person. Give them a boat
-            // but don't move the j counter.
+            // but don't move the j index.
             else {
                 boatCount++;
             }
         }
         
-
         return boatCount;
     }
 
